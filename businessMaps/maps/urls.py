75d8ae88b6b6a6be6a8e351aa5691c4cf48fc0b1
2splashes <dkn22@cam.ac.uk>
2017-03-04 23:32:44 +0000

    views update

--- businessMaps/maps/urls.py
+++ businessMaps/maps/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns= [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.Dashboard.as_view(), name='index'),
 	url('^ajax/', views.ajax, name='ajax'),
 ]
--- businessMaps/maps/views.py
+++ businessMaps/maps/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 # Google maps API example
 from django.conf import settings
@@ -9,25 +9,25 @@ import json
 from . import models
 from django.http import HttpResponse
 
-def index(request):
-	gmaps = models.MapRequest().g_maps()
-	test_variable='Test string to pass to the template'
-	template=loader.get_template('maps/index.html')
-	context={
-		'test_variable': test_variable,
-		'lat': gmaps.lat,
-		'lng': gmaps.lng,
-		'geojson': gmaps.geolist,
-	}
+class Dashboard(CreateView):
+	def get(self, request):
+		gmaps = models.MapRequest().g_maps()
+		test_variable='Test string to pass to the template'
+		template=loader.get_template('maps/index.html')
+		context={
+			'test_variable': test_variable,
+			'lat': gmaps.lat,
+			'lng': gmaps.lng,
+			'geojson': gmaps.geolist,
+		}
 
-	return render(request,"maps/index.html",context)
-	# return HttpResponse("Hello, world.")
+		return render(request,"maps/index.html", context)
+		# return HttpResponse("Hello, world.")
 
 def ajax(request):
 	gmaps = models.MapRequest().g_maps()
 	geojson = json.dumps(gmaps.geolist)
 	return HttpResponse(geojson, content_type = 'application/json') 
-# latest
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
